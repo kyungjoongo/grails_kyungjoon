a6231db93952ae237468f3544cfe1cf1c5b7a1b0
kyungjoongo <kyungjoon.go@gmail.com>
2017-07-06 13:59:19 +0900

    imageupload 추가

--- grails-app/controllers/grails_kyungjoon/TestController.groovy
+++ grails-app/controllers/grails_kyungjoon/TestController.groovy
@@ -1,8 +1,5 @@
 package grails_kyungjoon
 
-import grails.web.context.ServletContextHolder
-import org.imgscalr.Scalr
-import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.servlet.ModelAndView
 
 import javax.imageio.ImageIO
@@ -14,22 +11,26 @@ class TestController {
     /*static scaffold = Test*/
     static defaultAction = "list"
 
+    final static imagePath = "e:/test/"
+
     def writeForm() {
     }
 
 
     def list() {
-
         List testList = Test.listOrderById(order: desc);
         return new ModelAndView("/test/list", [testList: testList])
     }
 
-
+    /**
+     *  upload image output stream
+     * @return
+     */
     def getImage() {
 
         def name = params.get("name");
 
-        def path = "e:/test/" + name
+        def path = imagePath + name
         //returns an image to display
         BufferedImage originalImage = ImageIO.read(new File(path));
 
@@ -60,8 +61,6 @@ class TestController {
         def test = new Test(name: name, content: content, pubDate: new Date(), modDate: new Date(), imageName: fileName)
         test.save(flush: true, failOnError: true);
 
-
-
         if (f.empty) {
             flash.message = 'file cannot be empty'
             render(view: 'uploadForm')
@@ -69,9 +68,6 @@ class TestController {
         } else {
 
             f.transferTo(new File('e:/test/' + fileName))
-
-            /*List testList = Test.getAll()*/
-
             List testList = Test.listOrderById(order: "desc");
             return new ModelAndView("/test/list", [testList: testList])
         }
@@ -79,13 +75,9 @@ class TestController {
     }
 
 
-    private static final File IMAGES_DIR = new File('/main/webapp/images')
-
     //get the list of files, to create links in the view
     def listImages() {
-
         List listFiles = IMAGES_DIR.listFiles()
-
         [images: listFiles]
     }
 
